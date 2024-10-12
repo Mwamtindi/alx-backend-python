8ddcca8427445ee002237a4c380fe429b243313a
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-12 10:54:40 +0300

    Basic annotations-to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+A module with a type-annotated fxn to_str that convert a floating-pointn to str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to a string.
+
+    Args:
+        n (float): The floating-point number to convert.
+
+    Returns:
+        str: The string representation of the floating-point number.
+    """
+    return str(n)
